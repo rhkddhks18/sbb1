0acf7ef910c60e7146eed0daf70268ef40aa0187
ko <139303842+rhkddhks18@users.noreply.github.com>
2023-11-24 02:52:56 +0900

    init

---
+++ src/main/java/com/example/sbb/article/Article.java
@@ -0,0 +1,23 @@
+package com.example.sbb.article;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 200)
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime createDate;
+
+}
---
+++ src/main/java/com/example/sbb/article/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.sbb.article;
+
+import com.example.sbb.article.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+}
